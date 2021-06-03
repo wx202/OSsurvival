580be62f64d03ddb0ad79ee89c441bd50bcd15b1
Xuan Wang <xwang@hsph.harvard.edu>
2021-06-03 09:33:05 -0400

    bb

--- example.R
+++ example.R
@@ -1,4 +1,5 @@
 library(devtools)
+devtools::install_github("wx202/OptSurrogateSurv")
 library(OptSurrogateSurv)
 
 load("~/Desktop/RPackages/OptSurrogateSurv/data/sysdata.rda")
@@ -8,7 +9,6 @@ xob=data.example$data$xob
 s.ob=data.example$data$s.ob
 deltaob=data.example$data$deltaob
 aob=data.example$data$aob
-
 
 
 out=pte.survival(xob,s.ob,deltaob,aob,t,t.0,varind=1,re=10)
